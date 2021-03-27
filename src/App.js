53e72f9694f1f1cf9c764b66b88c2f0e576a8078
Siddharth Shah <siddharthshah23@outlook.com>
2021-03-27 02:10:03 -0400

    added signup form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Signup from "../src/layout/Signup";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+};
+
+export default App;
