f4900238193a71d0dd5dcf906d88153aa57cf898
Siddharth Shah <siddharthshah23@outlook.com>
2021-03-29 01:15:06 -0400

    Admin can update the roles

--- src/auth/Admin.js
+++ src/auth/Admin.js
@@ -1,11 +1,192 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { isAuth, getCookie, signout, updateUser } from "../auth/Helper";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { ToastContainer, toast } from "material-react-toastify";
+import "material-react-toastify/dist/ReactToastify.css";
 
-const Admin = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Admin({ history }) {
+  const classes = useStyles();
+
+  const [values, setValues] = useState({
+    role: "",
+    name: "",
+    email: "",
+    password: "",
+    buttonText: "Submit",
+  });
+
+  const token = getCookie("token");
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  const loadProfile = () => {
+    axios({
+      method: "GET",
+      url: `${process.env.REACT_APP_API}/user/${isAuth()._id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        console.log("ADMIN PROFILE UPDATE", response);
+        const { role, name, email } = response.data;
+        setValues({ ...values, role, name, email });
+      })
+      .catch((error) => {
+        console.log("ADMIN PROFILE UPDATE ERROR", error.response.data.error);
+        if (error.response.status === 401) {
+          signout(() => {
+            history.push("/");
+          });
+        }
+      });
+  };
+
+  const { role, name, email, password, buttonText } = values;
+
+  const handleChange = (name) => (event) => {
+    // console.log(event.target.value);
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submit");
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "PUT",
+      url: `${process.env.REACT_APP_API}/admin/update`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { name, password, role },
+    })
+      .then((response) => {
+        console.log("ADMIN PROFILE UPDATE SUCCESS", response);
+        updateUser(response, () => {
+          toast.success("Profile updated successfully");
+          setValues({ ...values, buttonText: "Submitted" });
+        });
+      })
+      .catch((error) => {
+        console.log("ADMIN PROFILE UPDATE ERROR", error.response.data.error);
+        setValues({ ...values, buttonText: "Submit" });
+        toast.error(error.response.data.error);
+      });
+  };
+
   return (
-    <div>
-      <h1>Admin</h1>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <ToastContainer position="top-center" autoClose={3000} />
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Admin Update Profile
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                name="name"
+                label="Name"
+                autoComplete="name"
+                onChange={handleChange("name")}
+                autoFocus
+                value={name}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                value={email}
+                autoComplete="email"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="role"
+                label="Role"
+                name="role"
+                value={role}
+                autoComplete="role"
+                onChange={handleChange("role")}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={handleChange("password")}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={clickSubmit}
+          >
+            {buttonText}
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}></Box>
+    </Container>
   );
-};
-
-export default Admin;
+}
